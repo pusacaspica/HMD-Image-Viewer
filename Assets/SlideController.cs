32e74cbe9ceb6a2fa06875bc59c9e3e4b0db5fdc
pusacaspica <horacio97@gmail.com>
2025-01-27 11:53:04 -0300

    Bugfixes all over the place
    There is a much necessary bugfix among these

--- Assets/SlideController.cs
+++ Assets/SlideController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
+using UnityEngine.Video;
 using UnityEngine;
 
 public class SlideController : MonoBehaviour
@@ -18,34 +19,49 @@ public class SlideController : MonoBehaviour
     // prefix1 = "img"
     // prefix2 = "alt"
     // fomrat = "jpg"
+    public bool flipping;
     public string path, prefix1, prefix2, format;
-    
+    public float ElapsedTime, RestingTime;
+    public int previousFrame;
     public Material imageOnDisplay;
-    public Texture2D debugImage;
-    public List<List<Texture2D>> Images;
+    public Texture2D debugClip;
+    public Material clipOnDisplay;
+    public List<List<Texture2D>> Clips;
     public TestRoutineController testRoutineController;
 
-    [NonSerialized] public int frame = 0;
+    public int frame = 0;
 
     public bool foveated = false;
 
+    //NOTE TO SELF
+    // images go inside the Resources folder
+    // format MUST NOT BE empty
+    //
+    // that's how Resources.Load roll
+
     void Awake()
     {
+        flipping = false;
+        clipOnDisplay = this.GetComponent<MeshRenderer>().material;
         testRoutineController = this.GetComponent<TestRoutineController>();
-        Images = new List<List<Texture2D>>();
-        path = Application.dataPath; 
+        RestingTime = testRoutineController.restingTime;
+        Clips = new List<List<Texture2D>>();
+        path = Application.dataPath + "/"; 
         for(int i = 0; i < imageCount; i++)
         {
-            List<Texture2D> pair = new List<Texture2D>();
-            //Debug.Log((Texture2D)AssetDatabase.LoadAssetAtPath(
-            //    "Assets/Images/raytraced_frame" + (i + 1).ToString() + ".png", typeof(Texture2D)));
-            pair.Add(Resources.Load<Texture2D>(prefix1 + (i + 1).ToString()));
-            Debug.Log(pair[0]);
-            //pair.Add((Texture2D)AssetDatabase.LoadAssetAtPath(path + "/" + prefix1 + (i + 1).ToString() + "." + format, typeof(Texture2D)));
-            pair.Add(Resources.Load<Texture2D>(prefix2 + (i + 1).ToString()));
-            Debug.Log(pair[1]);
-            //pair.Add((Texture2D)AssetDatabase.LoadAssetAtPath(path + "/" + prefix2 + (i + 1).ToString() + "." + format, typeof(Texture2D)));
-            Images.Add(pair);
+            List<Texture2D> pair = new()
+            {
+                //Debug.Log(path + prefix1 + (i+1).ToString() + format);
+                //Debug.Log((VideoClip)AssetDatabase.LoadAssetAtPath(
+                //    "Assets/Images/raytraced_frame" + (i + 1).ToString() + ".png", typeof(VideoClip)));
+                Resources.Load<Texture2D>(prefix1 + (i + 1).ToString() ),
+                //Debug.Log(pair[0]);
+                //pair.Add((VideoClip)AssetDatabase.LoadAssetAtPath(path + "/" + prefix1 + (i + 1).ToString() + "." + format, typeof(VideoClip)));
+                Resources.Load<Texture2D>(prefix2 + (i + 1).ToString() )
+            };
+            //Debug.Log(pair[1]);
+            //pair.Add((VideoClip)AssetDatabase.LoadAssetAtPath(path + "/" + prefix2 + (i + 1).ToString() + "." + format, typeof(VideoClip)));
+            Clips.Add(pair);
             
             //Debug.Log("added: Images/raytraced_frame" + (i + 1).ToString() + ".png");
             //Debug.Log("added: Images/foveated_frame" + (i + 1).ToString() + ".png");
@@ -53,61 +69,109 @@ public class SlideController : MonoBehaviour
 
         frame = -1;
 
-        Debug.Log(Images.Count.ToString() + " images loaded");
+        Debug.Log(Clips);
+        Debug.Log(Clips.Count.ToString() + " pairs loaded");
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        if (frame == -1) imageOnDisplay.mainTexture = debugImage;
-        else imageOnDisplay.mainTexture = Images[frame][foveated ? 1 : 0];
+        ElapsedTime += Time.deltaTime;
+        if(flipping){
+            if(ElapsedTime >= RestingTime){
+                    frame = previousFrame;
+                    foveated = !foveated;
+                    flipping = false;
+            }
+        }
+        if (frame == -1) {
+            clipOnDisplay.mainTexture = debugClip;
+        }
+        else {
+            clipOnDisplay.mainTexture = Clips[frame][foveated ? 1 : 0];
+        }
 
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            frame = (frame == 0) ? Images.Count - 1 : frame-1;
+            frame = (frame == 0) ? Clips.Count - 1 : frame-1;
             //Debug.Log("_frame is "+frame.ToString());
         }
         else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            frame = (frame == (Images.Count - 1)) ? 0 : frame+1;
+            frame = (frame == (Clips.Count - 1)) ? 0 : frame+1;
             //Debug.Log("_frame is "+frame.ToString());
         }
         
         if(Input.GetKeyDown(KeyCode.F) || Input.GetKeyDown(KeyCode.Backspace))
         {
-            foveated = !foveated;
+            previousFrame = frame;
+            frame = -1;
+            ElapsedTime = 0f;
+            flipping =true;
+            //foveated = !foveated;
             //Debug.Log("foveated is "+foveated.ToString());
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha0) ||
-            Input.GetKeyDown(KeyCode.Keypad0)) {frame = 0;
-            Debug.Log("_frame is "+frame.ToString());}
+            Input.GetKeyDown(KeyCode.Keypad0))
+        {
+            frame = 0;
+            //Debug.Log("_frame is "+frame.ToString());
+        }
         if (Input.GetKeyDown(KeyCode.Alpha1) ||
-            Input.GetKeyDown(KeyCode.Keypad1)) {frame = 1;
-            Debug.Log("_frame is "+frame.ToString());}
+            Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            frame = 1;
+            //Debug.Log("_frame is "+frame.ToString());
+        }
         if (Input.GetKeyDown(KeyCode.Alpha2) ||
-            Input.GetKeyDown(KeyCode.Keypad2)) {frame = 2;
-            Debug.Log("_frame is "+frame.ToString());}
+            Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            frame = 2;
+            //Debug.Log("_frame is "+frame.ToString());
+        }
         if (Input.GetKeyDown(KeyCode.Alpha3) ||
-            Input.GetKeyDown(KeyCode.Keypad3)) {frame = 3;
-            Debug.Log("_frame is "+frame.ToString());}
+            Input.GetKeyDown(KeyCode.Keypad3))
+        {
+            frame = 3;
+            //Debug.Log("_frame is "+frame.ToString());
+        }
         if (Input.GetKeyDown(KeyCode.Alpha4) ||
-            Input.GetKeyDown(KeyCode.Keypad4)) {frame = 4;
-            Debug.Log("_frame is "+frame.ToString());}
-        if (Input.GetKeyDown(KeyCode.Alpha5) ||
-            Input.GetKeyDown(KeyCode.Keypad5)) {frame = 5;
-            Debug.Log("_frame is "+frame.ToString());}
+            Input.GetKeyDown(KeyCode.Keypad4))
+        {
+            frame = 4;
+            //Debug.Log("_frame is " + frame.ToString());
+        }
+        /*if (Input.GetKeyDown(KeyCode.Alpha5) ||
+            Input.GetKeyDown(KeyCode.Keypad5))
+        {
+            frame = 5;
+            //Debug.Log("_frame is " + frame.ToString());
+        }
         if (Input.GetKeyDown(KeyCode.Alpha6) ||
-            Input.GetKeyDown(KeyCode.Keypad6)) {frame = 6;
-            Debug.Log("_frame is "+frame.ToString());}
+            Input.GetKeyDown(KeyCode.Keypad6))
+        {
+            frame = 6;
+            //Debug.Log("_frame is " + frame.ToString());
+        }
         if (Input.GetKeyDown(KeyCode.Alpha7) ||
-            Input.GetKeyDown(KeyCode.Keypad7)) {frame = 7;
-            Debug.Log("_frame is "+frame.ToString());}
+            Input.GetKeyDown(KeyCode.Keypad7))
+        {
+            frame = 7;
+            //Debug.Log("_frame is " + frame.ToString());
+        }
         if (Input.GetKeyDown(KeyCode.Alpha8) ||
-            Input.GetKeyDown(KeyCode.Keypad8)) {frame = 8;
-            Debug.Log("_frame is "+frame.ToString());}
+            Input.GetKeyDown(KeyCode.Keypad8))
+        {
+            frame = 8;
+            //Debug.Log("_frame is " + frame.ToString());
+        }
         if (Input.GetKeyDown(KeyCode.Alpha9) ||
-            Input.GetKeyDown(KeyCode.Keypad9)) {frame = 9;
-            Debug.Log("_frame is "+frame.ToString());}
+            Input.GetKeyDown(KeyCode.Keypad9))
+        {
+            frame = 9;
+            //Debug.Log("_frame is " + frame.ToString());
+        }*/
     }
 }
--- Assets/TestRoutineController.cs
+++ Assets/TestRoutineController.cs
@@ -4,22 +4,28 @@ using System.Collections.Generic;
 using TMPro.SpriteAssetUtilities;
 using Unity.VisualScripting;
 using UnityEngine;
+using TMPro;
 using Random = UnityEngine.Random;
 
 public class TestRoutineController : MonoBehaviour
 {
     public float ElapsedTime, ElapsedRoundTime;
-    public int images, repeats;
+    public int images, repeats, pastImages;
     public float exposureTime, restingTime;
     public bool testInSession, resting, voting;
     public List<int> RoundIndexes;
+    public List<string> EffectIndexes;
+    public TMP_Text textBox;
 
-    private SlideController _slideController;
+    public List<GameObject> crosshair;
 
-    private int _indexOnScreen, _prevIndexOnScreen;
-    // Start is called before the first frame update
-    void Awake()
+    public SlideController _slideController;
+
+    public int _indexOnScreen, _prevIndexOnScreen;
+
+    public void ResetTestRoutine()
     {
+        pastImages = 0;
         _slideController = this.GetComponent<SlideController>();
         RoundIndexes = new List<int>();
         
@@ -35,17 +41,38 @@ public class TestRoutineController : MonoBehaviour
             for (int j = 0; j < repeats; j++)
             {
                 RoundIndexes.Add(i);
+                switch(i){
+                    case 0:
+                        EffectIndexes.Add("Subsurface Scattering");
+                        break;
+                    case 1:
+                        EffectIndexes.Add("Emission");
+                        break;
+                    case 2:
+                        EffectIndexes.Add("Colored Shadows");
+                        break;
+                    case 3:
+                        EffectIndexes.Add("Refraction");
+                        break;
+                    default:
+                        EffectIndexes.Add("Reflections");
+                        break;
+                }
             }
         }
 
         _indexOnScreen = -1;
+    }
+    // Start is called before the first frame update
+    void Awake()
+    {
+        ResetTestRoutine();
         _prevIndexOnScreen = -1;
         Debug.Log(RoundIndexes.Count.ToString());
-    }
 
-    void Start()
-    {
-        
+
+        //textBox.SetText("N. of Images: ", images);
+        //textBox.SetText("Current Image: ");
     }
 
     // Update is called once per frame
@@ -56,7 +83,17 @@ public class TestRoutineController : MonoBehaviour
         // starts next round; elect new image and start the routine again
         if (Input.GetKeyDown(KeyCode.S))
         {
-            while(_prevIndexOnScreen == _indexOnScreen) _indexOnScreen = (int) Random.Range(0, RoundIndexes.Count);
+            if(_prevIndexOnScreen == _indexOnScreen) {
+                if(RoundIndexes.Count == 0){
+                    ResetTestRoutine();
+                    _prevIndexOnScreen = -1;
+                    return;
+                }
+                else if(RoundIndexes.Count == 1) 
+                    _indexOnScreen = 0;
+                else
+                    _indexOnScreen = (int) Random.Range((int)0, (int)RoundIndexes.Count);
+            }
             _slideController.frame = RoundIndexes[_indexOnScreen];
             Debug.Log(_indexOnScreen.ToString());
             ElapsedRoundTime = 0f;
@@ -65,82 +102,56 @@ public class TestRoutineController : MonoBehaviour
             voting = false;
         }
 
+        if (Input.GetKeyDown(KeyCode.H)){
+            for(int i=0; i<crosshair.Count;i++){
+                crosshair[i].SetActive(!crosshair[i].activeSelf);
+            }
+        }
+
         if (testInSession)
         {
-            // RESTING ROUTINE
-            // when resting time is over, change from raytraced to foveated;
-            /*if (resting)
+            textBox.SetText("Elapsed time: " + ElapsedTime.ToString()+"\n"+"Images: " + pastImages.ToString()+ "/" + (images * repeats).ToString() +"\nEffect on display: " + EffectIndexes[_indexOnScreen] + "\nFoveated? " + _slideController.foveated.ToString());
+            if (!resting && !voting)
             {
+                _slideController.frame = RoundIndexes[_indexOnScreen];
+                textBox.SetText("Elapsed time: " + ElapsedTime.ToString()+"\n"+"Images: " + pastImages.ToString()+ "/" + (images * repeats).ToString() +"\nEffect on display: " + EffectIndexes[_indexOnScreen] + "\nFoveated? " + _slideController.foveated.ToString());
+                if (ElapsedRoundTime >= exposureTime)
+                {
+                    if (!_slideController.foveated)
+                    {
+                        ElapsedRoundTime = 0f;
+                        resting = true;
+                    }
+                    else
+                    {
+                        _slideController.foveated = false;
+                        ElapsedRoundTime = 0f;
+                        _prevIndexOnScreen = _indexOnScreen;
+                        RoundIndexes.RemoveAt(_prevIndexOnScreen);
+                        EffectIndexes.RemoveAt(_prevIndexOnScreen);
+                        _slideController.frame = -1;
+                        pastImages++;
+                        voting = true;
+                    }
+                }
+            }
+            else if(resting && !voting)
+            {
+                //textBox.SetText("Images: " + pastImages.ToString()+ "/" + (images * repeats).ToString() +"\nEffect on display: " + EffectIndexes[_indexOnScreen] + "\nFoveated? " + _slideController.foveated.ToString());
                 _slideController.frame = -1;
                 if (ElapsedRoundTime >= restingTime)
                 {
+                    ElapsedRoundTime = 0f;
                     resting = false;
-                    ElapsedRoundTime = 0.0f;
-                    _slideController.foveated = true;
+                    _slideController.foveated = !_slideController.foveated;
                 }
             }
-            // EXPOSURE ROUTINE
-            // when exposure time is over, if the image was raytraced, start resting routine
-            // when exposure time is over, if the image was foveated, pick a new image to show and wait for input
-            else
-            {
-                if (ElapsedRoundTime >= exposureTime || !_slideController.foveated)
-                {
-                    resting = true;
-                    ElapsedRoundTime = 0.0f;
-                }
-
-                if (ElapsedRoundTime >= exposureTime || _slideController.foveated)
-                {
-                    ElapsedRoundTime = 0.0f; //reset elapsed time
-                    
-                    _slideController.foveated = false; //confirm that next round will start by raytraced image
-                    
-                    // end round
-                    _prevIndexOnScreen = _indexOnScreen;
-                    int item = RoundIndexes[_prevIndexOnScreen];
-                    RoundIndexes.Remove(item);
-                    _slideController.frame = -1;
-                }
-                
-                _slideController.frame = RoundIndexes[_indexOnScreen];*/
-        //}
-
-        if (!resting && !voting)
-        {
-            Debug.Log("index on screen: " + _indexOnScreen.ToString());
-            _slideController.frame = RoundIndexes[_indexOnScreen];
-            if (ElapsedRoundTime >= exposureTime)
-            {
-                if (!_slideController.foveated)
-                {
-                    ElapsedRoundTime = 0f;
-                    resting = true;
-                }
-                else
-                {
-                    _slideController.foveated = false;
-                    ElapsedRoundTime = 0f;
-                    _prevIndexOnScreen = _indexOnScreen;
-                    RoundIndexes.RemoveAt(_prevIndexOnScreen);
-                    _slideController.frame = -1;
-                    voting = true;
-                }
+            else if(!resting && voting){
+                //textBox.SetText("Images: " + pastImages.ToString()+ "/" + (images * repeats).ToString() +"\nEffect on display: " + EffectIndexes[_indexOnScreen] + "\nFoveated? " + _slideController.foveated.ToString());
             }
-        }
-        else if(resting && !voting)
-        {
-            _slideController.frame = -1;
-            if (ElapsedRoundTime >= restingTime)
-            {
-                ElapsedRoundTime = 0f;
-                resting = false;
-                _slideController.foveated = !_slideController.foveated;
-            }
-        }
 
-        ElapsedTime += Time.deltaTime;
-            ElapsedRoundTime += Time.deltaTime;
+            ElapsedTime += Time.deltaTime;
+                ElapsedRoundTime += Time.deltaTime;
         }
     }
 }
